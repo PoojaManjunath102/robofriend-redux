646b48054b4d1c3caf0d34c167c9204501b1d936
PoojaManjunath102 <37259784+PoojaManjunath102@users.noreply.github.com>
2020-06-10 23:14:47 +0530

    Add files via upload

---
+++ build/precache-manifest.baebbf9499815282c1c6ff2bef73ea99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9570fec9b477ec81bda4eaa7fb58fabd",
+    "url": "/robofriend-redux/index.html"
+  },
+  {
+    "revision": "357fab4b8d45a6f6ade0",
+    "url": "/robofriend-redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "993b9aaf590ca5c618cf",
+    "url": "/robofriend-redux/static/css/main.17c270a3.chunk.css"
+  },
+  {
+    "revision": "357fab4b8d45a6f6ade0",
+    "url": "/robofriend-redux/static/js/2.ed32bacb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriend-redux/static/js/2.ed32bacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993b9aaf590ca5c618cf",
+    "url": "/robofriend-redux/static/js/main.a280af4d.chunk.js"
+  },
+  {
+    "revision": "e72a00cf859d293fb3f7",
+    "url": "/robofriend-redux/static/js/runtime-main.fe513f9f.js"
+  },
+  {
+    "revision": "61806d9c7c908f1b4340fb5326d4b52a",
+    "url": "/robofriend-redux/static/media/SEGA.61806d9c.woff"
+  }
+]);
